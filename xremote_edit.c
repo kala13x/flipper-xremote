687a133e03aadb963bb0b0af6aa07f98393e7cd4
Sandro Kalatozishvili <kala0x13@gmail.com>
2023-10-19 21:36:56 +0400

    Finished custom layout

--- xremote_edit.c
+++ xremote_edit.c
@@ -33,16 +33,86 @@ static void xremote_edit_buttons_store(XRemoteAppButtons* buttons) {
     xremote_app_extension_store(buttons, app_ctx->file_path);
 }
 
-static void xremote_edit_up_press_changed(VariableItem* item) {
+static void xremote_item_update_item(VariableItem* item, FuriString* button) {
     XRemoteEditContext* ctx = variable_item_get_context(item);
     XRemoteAppButtons* buttons = ctx->buttons;
 
     int button_index = variable_item_get_current_value_index(item);
     const char* button_name = xremote_button_get_name(button_index);
-
-    furi_string_set_str(buttons->custom_up, button_name);
     variable_item_set_current_value_text(item, button_name);
+
+    furi_string_set_str(button, button_name);
     xremote_edit_buttons_store(buttons);
+}
+
+static void xremote_edit_ok_press_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_ok);
+}
+
+static void xremote_edit_up_press_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_up);
+}
+
+static void xremote_edit_down_press_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_down);
+}
+
+static void xremote_edit_left_press_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_left);
+}
+
+static void xremote_edit_right_press_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_right);
+}
+
+static void xremote_edit_ok_hold_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_ok_hold);
+}
+
+static void xremote_edit_up_hold_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_up_hold);
+}
+
+static void xremote_edit_down_hold_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_down_hold);
+}
+
+static void xremote_edit_left_hold_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_left_hold);
+}
+
+static void xremote_edit_right_hold_changed(VariableItem* item) {
+    XRemoteEditContext* ctx = variable_item_get_context(item);
+    xremote_item_update_item(item, ctx->buttons->custom_right_hold);
+}
+
+static void xremote_edit_list_add_item(
+    XRemoteEditContext* context,
+    const char* item_name,
+    FuriString* button,
+    VariableItemChangeCallback change_callback) {
+    VariableItemList* list = context->item_list;
+    VariableItem* item;
+
+    /* Add custom_up to variable item list */
+    item = variable_item_list_add(list, item_name, XREMOTE_BUTTON_COUNT, change_callback, context);
+
+    /* Get button name and index */
+    const char* button_name = furi_string_get_cstr(button);
+    uint32_t button_index = xremote_button_get_index(button_name);
+
+    /* Set button name and index to the list item */
+    variable_item_set_current_value_index(item, button_index);
+    variable_item_set_current_value_text(item, button_name);
 }
 
 static XRemoteEditContext* xremote_edit_context_alloc(XRemoteAppButtons* buttons) {
@@ -57,21 +127,27 @@ static XRemoteEditContext* xremote_edit_context_alloc(XRemoteAppButtons* buttons
     view_set_previous_callback(view, xremote_edit_view_exit_callback);
     view_dispatcher_add_view(app_ctx->view_dispatcher, XRemoteViewIRCustomEditPage, view);
 
-    /* Add custom_up to variable item list */
-    VariableItem* item = variable_item_list_add(
-        context->item_list,
-        "Up press",
-        XREMOTE_BUTTON_COUNT,
-        xremote_edit_up_press_changed,
-        context);
-
-    /* Get button name and index */
-    const char* button_name = furi_string_get_cstr(buttons->custom_up);
-    uint32_t button_index = xremote_button_get_index(button_name);
-
-    /* Set button name and index to the list item */
-    variable_item_set_current_value_index(item, button_index);
-    variable_item_set_current_value_text(item, button_name);
+    /* Add press items to the variable list */
+    xremote_edit_list_add_item(
+        context, "Ok press", buttons->custom_ok, xremote_edit_ok_press_changed);
+    xremote_edit_list_add_item(
+        context, "Up press", buttons->custom_up, xremote_edit_up_press_changed);
+    xremote_edit_list_add_item(
+        context, "Down press", buttons->custom_down, xremote_edit_down_press_changed);
+    xremote_edit_list_add_item(
+        context, "Left press", buttons->custom_left, xremote_edit_left_press_changed);
+    xremote_edit_list_add_item(
+        context, "Right press", buttons->custom_right, xremote_edit_right_press_changed);
+    xremote_edit_list_add_item(
+        context, "Ok hold", buttons->custom_ok_hold, xremote_edit_ok_hold_changed);
+    xremote_edit_list_add_item(
+        context, "Up hold", buttons->custom_up_hold, xremote_edit_up_hold_changed);
+    xremote_edit_list_add_item(
+        context, "Down hold", buttons->custom_down_hold, xremote_edit_down_hold_changed);
+    xremote_edit_list_add_item(
+        context, "Left hold", buttons->custom_left_hold, xremote_edit_left_hold_changed);
+    xremote_edit_list_add_item(
+        context, "Right hold", buttons->custom_right_hold, xremote_edit_right_hold_changed);
 
     return context;
 }
